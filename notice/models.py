e6adf590ebcf0079268500158db509ac36708fef
GrigoriySZ <zaitsevgs@gmail.com>
2026-09-20 22:03:17 +0700

    feat: add Notice model

--- notice/models.py
+++ notice/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Notice(models.Model):
+    title = models.CharField(
+        max_length=100,
+        verbose_name='Заголовок',
+        help_text='Введите заголовок объявления'
+    )
+    content = models.TextField(
+        verbose_name='Текст объявления',
+        help_text='Введите тектс объявления'
+    )
+    authore_name = models.CharField(
+        max_length=50,
+        verbose_name='Имя автора',
+        help_text='Введите имя автора объявления'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата создания'
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+            verbose_name = "Объявление"
+            verbose_name_plural = "Объявления"
+
+            ordering = ['-created_at']
