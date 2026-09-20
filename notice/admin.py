eb6e06e38f83d9451600147ca72f0599c1ba9968
GrigoriySZ <zaitsevgs@gmail.com>
2026-09-20 22:12:14 +0700

    feat: register Notice in admin panel

--- notice/admin.py
+++ notice/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Notice
 
-# Register your models here.
+@admin.register(Notice)
+class NoticeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author_name', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('title',)
